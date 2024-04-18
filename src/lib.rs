4afa28efb8c10052669d002825de2d5b20eaf819
Bluheir <slimcognitodudesvids@gmail.com>
2024-04-18 22:32:53 +1000

    Change API a bit

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,34 @@
 pub mod obj;
 pub mod mock;
 pub mod node;
+#[cfg(test)]
+mod tests;
 
 use core::net::SocketAddr;
+use std::{error::Error as StdError, sync::Arc};
 
 use futures::Future;
 use obj::{ReqMessage, RespMessage};
 use tokio::io::{AsyncRead, AsyncWrite};
 
-pub trait Endpoint {
-    type Conn;
-    type Err;
+pub const CURRENT_VERSION: u32 = 0;
 
-    fn connect(&self, domain: &str, addr: SocketAddr) -> impl Future<Output = Result<Self::Conn, Self::Err>>;
-    fn accept(&self) -> impl Future<Output = Result<Self::Conn, Self::Err>>;
+pub trait Endpoint {
+    type Conn: Connection;
+    type Error: StdError;
+
+    fn connect(&self, domain: &str, addr: SocketAddr) -> impl Future<Output = Result<Self::Conn, Self::Error>>;
+    fn accept(&self) -> impl Future<Output = Result<Self::Conn, Self::Error>>;
 }
 
 pub trait Connection {
-    type Request: Request;
+    type Responder: Request;
 
     type Read: AsyncRead;
     type Write: AsyncWrite;
 
-    type StreamError;
-    type ReqError;
+    type StreamError: StdError;
+    type ReqError: StdError;
 
     /// Waits for the next stream to be opened by the endpoint.
     fn next_raw(&self) -> impl Future<Output = Result<Stream<Self::Write, Self::Read>, Self::StreamError>>;
@@ -33,19 +38,64 @@ pub trait Connection {
     fn open_raw(&self) -> impl Future<Output = Result<Stream<Self::Write, Self::Read>, Self::StreamError>>;
 
     /// Waits for the next request from the endpoint.
-    fn next_request(&self) -> impl Future<Output = Result<Self::Request, Self::ReqError>>;
+    fn next_request(&self) -> impl Future<Output = Result<(ReqMessage, Self::Responder), Self::ReqError>>;
     /// Sends a request to the endpoint.
     fn request(&self, req: ReqMessage) -> impl Future<Output = Result<RespMessage, Self::ReqError>>;
 }
 
-pub trait Request {
-    type Error;
+impl<T: Connection> Connection for &T {
+    type Responder = T::Responder;
 
-    fn request_msg(&self) -> &ReqMessage;
+    type Read = T::Read;
+    type Write = T::Write;
+
+    type StreamError = T::StreamError;
+    type ReqError = T::ReqError;
+
+    fn next_raw(&self) -> impl Future<Output = Result<Stream<Self::Write, Self::Read>, Self::StreamError>> {
+        (*self).next_raw()
+    }
+    fn open_raw(&self) -> impl Future<Output = Result<Stream<Self::Write, Self::Read>, Self::StreamError>> {
+        (*self).open_raw()
+    }
+    fn next_request(&self) -> impl Future<Output = Result<(ReqMessage, Self::Responder), Self::ReqError>> {
+        (*self).next_request()
+    }
+    fn request(&self, req: ReqMessage) -> impl Future<Output = Result<RespMessage, Self::ReqError>> {
+        (*self).request(req)
+    }
+}
+impl<T: Connection> Connection for Arc<T> {
+    type Responder = T::Responder;
+
+    type Read = T::Read;
+    type Write = T::Write;
+
+    type StreamError = T::StreamError;
+    type ReqError = T::ReqError;
+
+    fn next_raw(&self) -> impl Future<Output = Result<Stream<Self::Write, Self::Read>, Self::StreamError>> {
+        (**self).next_raw()
+    }
+    fn open_raw(&self) -> impl Future<Output = Result<Stream<Self::Write, Self::Read>, Self::StreamError>> {
+        (**self).open_raw()
+    }
+    fn next_request(&self) -> impl Future<Output = Result<(ReqMessage, Self::Responder), Self::ReqError>> {
+        (**self).next_request()
+    }
+    fn request(&self, req: ReqMessage) -> impl Future<Output = Result<RespMessage, Self::ReqError>> {
+        (**self).request(req)
+    }
+}
+
+pub trait Request {
+    type Error: StdError;
+
     fn respond(self, resp: RespMessage) -> impl Future<Output = Result<(), Self::Error>>;
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Default, Hash, serde::Serialize, serde::Deserialize)]
 pub struct Stream<Write, Read> {
-    pub read: Read,
     pub write: Write,
+    pub read: Read,
 }
