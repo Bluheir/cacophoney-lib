159a3dcba195974b34727ce9a611f5987887e95e
Bluheir <slimcognitodudesvids@gmail.com>
2024-05-16 18:03:18 +1000

    Add MockConnection

--- src/mock/mod.rs
+++ src/mock/mod.rs
@@ -4,8 +4,9 @@ use futures::Future;
 pub use stream::*;
 use tokio::sync::mpsc;
 
-use crate::{crypto::KeyTriad, node::Notify, obj::SignedData};
+use crate::{crypto::{KeyTriad, PublicKey}, node::Notify, obj::SignedData};
 
+#[derive(Clone, Debug)]
 pub struct MockNotify {
     send: mpsc::Sender<KeyTriad<SignedData>>,
 }
@@ -19,4 +20,11 @@ impl Notify for MockNotify {
     ) -> impl Future<Output = Result<(), Self::Err>> + Send + Sync {
         self.send.send(triad.clone())
     }
+}
+
+pub struct MockConnection {
+    notify: MockNotify,
+    notify_recv: mpsc::Receiver<KeyTriad<SignedData>>,
+    stream_opener: mpsc::Sender<(PublicKey, MockWrite, MockRead)>,
+    
 }
